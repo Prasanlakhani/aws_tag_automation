8981f1ee49139cc8c3a70ca5c8216c633161d956
prasan <35463310+Prasanlakhani@users.noreply.github.com>
2024-01-05 16:13:52 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,32 @@
 variable "region_name" {
   type = string
-  default     = "ap-south-1"
-  #description = "The ID of the project where this VPC will be created"
-}
-
-variable "s3_tfstate_name" {
-  type = string
-  default     = "tagautomations3bucket-terraform-state-bucket"
-  description = "The ID of the project where this VPC will be created"
+  default     = "us-east-1"
+  description = "Aws Region"
 }
 
 variable "s3_name" {
   type = string
-  default     = "tagautomations3bucket"
-  description = "The ID of the project where this VPC will be created"
+  default     = "tagautomations3bucket_test"
+  description = "S3 bucket name, where the .json file will be stored"
 }
+
+variable "lambda_name" {
+  type = string
+  default     = "tag_automation_lambda_test"
+  description = "name of the lamda function"
+}
+
+variable "role_name" {
+  type = string
+  default     = "tag_automation_role_test"
+  description = "aws role name"
+}
+
+variable "policy_name" {
+  type = string
+  default     = "tag_automation_policy_test"
+  description = "aws policy name"
+}
+
+
+
